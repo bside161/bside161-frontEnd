7205738805323b29c9c95beb1b2a2b3ec69cb701
yuncs <changsoonyun@naver.com>
2023-09-04 10:04:08 +0900

    Feat : reset.css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './assets/fonts/SUIT-Variable.css';
+import './styles/reset.css'
 import { globalStyles } from './styles/globalStyles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
