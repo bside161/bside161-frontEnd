4325563d22bca7f5c8d8c7b483ac734a68e72e6a
yuncs <changsoonyun@naver.com>
2023-09-05 09:34:18 +0900

    Fix : 컬러변수명 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,18 @@
-export const colors = {
-  primary: '#5F27FF',
-  secondary: '#0DE361',
-  background: '#F5F6F8',
-  black: '#000000',
-  black87: 'rgba(0, 0, 0, 0.87)',
-  black73: 'rgba(0, 0, 0, 0.73)',
-  black60: 'rgba(0, 0, 0, 0.60)',
-  black40: 'rgba(0, 0, 0, 0.40)',
-  black33: 'rgba(0, 0, 0, 0.33)',
-  black15: 'rgba(0, 0, 0, 0.15)',
-  black10: 'rgba(0, 0, 0, 0.10)',
-  black5: 'rgba(0, 0, 0, 0.05)',
-  white: '#FFFFFF',
-  white95: 'rgba(255, 255, 255, 0.95)',
+const colors = {
+  c1: '#5F27FF',
+  c2: '#0DE361',
+  bg1: '#F5F6F8',
+  b: '#000000',
+  b2: 'rgba(0, 0, 0, 0.87)',
+  b4: 'rgba(0, 0, 0, 0.73)',
+  b6: 'rgba(0, 0, 0, 0.60)',
+  b9: 'rgba(0, 0, 0, 0.40)',
+  ba: 'rgba(0, 0, 0, 0.33)',
+  l1: 'rgba(0, 0, 0, 0.15)',
+  l2: 'rgba(0, 0, 0, 0.10)',
+  l3: 'rgba(0, 0, 0, 0.05)',
+  w1: '#FFFFFF',
+  w2: 'rgba(255, 255, 255, 0.95)',
 };
+
+export { colors };
