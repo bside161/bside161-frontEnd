8ca7148a35f04380eac8d91e9e1568d967ec3e1d
RendarCP <tjddnr0217@gmail.com>
2023-09-05 15:51:59 +0900

    Feat : 인기있는 아이디어 카드 및 header부분 수정
    - theme 적용 및 피드부분 header 추가

---
+++ src/components/Card/IdeaCard.tsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+const IdeaCard = () => {
+  return <CardContainer>IdeaCard</CardContainer>;
+}
+
+export default IdeaCard;
+
+const CardContainer = styled.div`
+  border: 1px solid red;
+  padding: 30px 20px;
+  border-radius: 8px;
+`
+
+const ProfileWrapper = styled.div`
+  
+`
+
+const ContentWrapper = styled.div`
+
+`
+
+const FooterWrapper = styled.div`
+  
+`
