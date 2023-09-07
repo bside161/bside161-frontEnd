411dbf8e49ff31955c9dbf47ca9a8179ce821778
RendarCP <tjddnr0217@gmail.com>
2023-09-07 16:16:22 +0900

    Feat : 컴포넌트 추가
    - bottomSheet 변경
    - Text 컴포넌트 추가
    - Spacer 추가

---
+++ src/components/Text.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { ReactNode, CSSProperties } from 'react';
+import { TypoType } from '../styles/theme';
+
+interface TextProps {
+  children: ReactNode;
+  font: CSSProperties;
+  color?: string;
+}
+
+const Text = ({ children, font, color }: TextProps) => {
+  return (
+    <TextBox color={color} style={{ ...font }}>
+      {children}
+    </TextBox>
+  );
+};
+
+export default Text;
+
+const TextBox = styled.p<{ color?: string }>`
+  color: ${(props) => (props.color ? props.color : props.theme.colors.b)};
+`;
