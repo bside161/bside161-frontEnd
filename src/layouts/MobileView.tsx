c4f7ee08c14382f9cc81482218afc49d084d00d0
shong <shong1332@naver.com>
2023-09-20 23:33:41 +0900

    feat : feeds 폴더 구조 추가 및 헤더 생성

--- src/layouts/MobileView.tsx
+++ src/layouts/MobileView.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
+
 import Navigation from '../components/Navigation';
 
 const MobileView = () => {
@@ -17,7 +18,8 @@ export default MobileView;
 
 const Container = styled.main`
   height: 100%;
-  max-width: 480px;
+  //피그마 크기인 375px로 조정했습니다. 확인 후 주석은 지우셔도 됩니다~
+  max-width: 375px;
   width: auto;
   position: relative;
   margin: 0 auto;
